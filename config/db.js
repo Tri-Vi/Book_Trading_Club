5870508f5185eb3457170192728c4ec495518f5c
TRI VI <trivi2710@gmail.com>
2017-11-05 16:28:12 -0500

    fix

--- config/db.js
+++ config/db.js
@@ -1,8 +1,3 @@
 module.exports = {
-<<<<<<< HEAD
   url: process.env.MONGODB_URI  
-=======
-  url: process.env.MONGODB_URI
-  // url: "mongodb://localhost:27017/book_trading_club"
->>>>>>> 2abeb1f08b647ab7b3ee9fa107a188e7343d3277
 }
